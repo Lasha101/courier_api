5f3191559b3d03c160b9d7855910f640dcdabb35
Lasha101 <gurshabo55@gmail.com>
2024-05-17 21:51:20 +0400

    add final commit

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,10 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import CustomUser, Parcel, DeliveryProof
 from .serializers import *
+from rest_framework.response import Response 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status= status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+       
 
 
 
